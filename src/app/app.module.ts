fed48be23d71440bd7daa8780f7b70c743194cf9
Kellie Higa <kelliekhiga@gmail.com>
2019-08-16 15:50:54 -0700

    Removed Home Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
