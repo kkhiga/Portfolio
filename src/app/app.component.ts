1eff629d993d07bdb2a56af19816948822b8753d
Kellie Higa <kelliekhiga@gmail.com>
2019-08-19 17:37:52 -0700

    Updated Nav Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio-kellie-higa';
+  hamburgerHover = false;
 
   constructor(public router: Router) {}
 }
