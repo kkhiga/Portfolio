c42f55f86b0041b59bc27ca6eeab6e10db976886
Kellie Higa <kelliekhiga@gmail.com>
2019-08-19 17:35:15 -0700

    Additional Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { ResumeComponent } from './resume/resume.component';
 import { WorkComponent } from './work/work.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PhotographyComponent } from './photography/photography.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'work', pathMatch: 'full' },
   { path: 'work', component: WorkComponent },
   { path: 'resume', component: ResumeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'photography', component: PhotographyComponent }
 ];
 
 @NgModule({
